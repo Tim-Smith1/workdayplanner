c15274565fc6ac1e6b2de4a32091ee77fa7bb00d
Tim Smith <meckaleck@hotmail.com>
2023-02-03 17:46:41 -0600

    got the color to change with the hour and items are storing in local storage need to make it look neater 2/3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,55 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn"t run until the browser has finished rendering all the elements
+// the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(function () {
 
-var todaysDate = dayjs().format("MMMM D, YYYY h:mm A");
-$("#currentDay").text(todaysDate);
-
-
-$(document).ready(function () {
-
-    $(".saveBtn").on("click", function () {
-        //console.log("hi");
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        //console.log("hello");
-
-        localStorage.setItem(time, text);
+  var current = dayjs("");
+  var current = dayjs();
+  $('#currentDay').text(current.format('MMM D YYYY hh:mm a'));
+  
+  $('.saveBtn').on  ('click', function(){
+    var text= $(this).siblings(".description").val();
+    var time=$(this).parent().attr("id");
+    localStorage.setItem(time, text);
+  })
+  
+  
+  function exactTime(){
+    var currentTime= dayjs().hour();
+    $(".time-block").each(function(){
+      var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
+      if (blockTime < currentTime){
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+         else if 
+        (blockTime === currentTime){
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+         else{
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+        
     })
+   
+  }
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
+        exactTime();
+  })
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -36,33 +68,3 @@ $(document).ready(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  }); 
-
-  function exactTime() {
-    var currentTime = dayjs().hour();
-    //console.log();
-    
-    $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-        if (blockTime < currentTime) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }  else if 
-            (blockTime === currentTime) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-
-        } else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
-
-
-
-    })
-        exactTime();
-}
